11537bb0d2bd975a35077c9df200ae9e9ce1b118
BadKarmaL33t <jchagesteijn@protonmail.com>
2023-11-20 21:20:27 +0100

    fixing major custom validator error in other entities

--- src/main/java/com/fsd/inventopilot/validations/impl/AllowedProductTypeImpl.java
+++ src/main/java/com/fsd/inventopilot/validations/impl/AllowedProductTypeImpl.java
@@ -1,16 +1,28 @@
 package com.fsd.inventopilot.validations.impl;
 
+import com.fsd.inventopilot.models.ProductType;
 import com.fsd.inventopilot.validations.AllowedProductType;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class AllowedProductTypeImpl implements ConstraintValidator<AllowedProductType, String> {
+public class AllowedProductTypeImpl implements ConstraintValidator<AllowedProductType, Object> {
+
     @Override
-    public void initialize(AllowedProductType allowedValue) {
+    public void initialize(AllowedProductType allowedProductType) {
     }
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        return "RAW".equals(value) || "SOLUBLE".equals(value);
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true; // null values are considered valid
+        }
+
+        if (value instanceof ProductType productType) { // Replace with the actual type used in your application
+            // Replace with the actual type used in your application
+            return ProductType.RAW.equals(productType) ||
+                    ProductType.SOLUBLE.equals(productType);
+        }
+
+        return false; // non-enum values are considered invalid
     }
 }
--- src/main/java/com/fsd/inventopilot/validations/impl/AllowedRoleImpl.java
+++ src/main/java/com/fsd/inventopilot/validations/impl/AllowedRoleImpl.java
@@ -1,16 +1,29 @@
 package com.fsd.inventopilot.validations.impl;
 
+import com.fsd.inventopilot.models.Role;
 import com.fsd.inventopilot.validations.AllowedRole;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
-public class AllowedRoleImpl implements ConstraintValidator<AllowedRole, String> {
+public class AllowedRoleImpl implements ConstraintValidator<AllowedRole, Object> {
+
     @Override
-    public void initialize(AllowedRole allowedValue) {
+    public void initialize(AllowedRole allowedRole) {
     }
 
     @Override
-    public boolean isValid(String value, ConstraintValidatorContext context) {
-        return "USER".equals(value) || "ADMIN".equals(value) || "SUPERUSER".equals(value);
+    public boolean isValid(Object value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true; // null values are considered valid
+        }
+
+        if (value instanceof Role role) { // Replace with the actual type used in your application
+            // Replace with the actual type used in your application
+            return Role.USER.equals(role) ||
+                    Role.SUPERUSER.equals(role) ||
+                    Role.ADMIN.equals(role);
+        }
+
+        return false; // non-enum values are considered invalid
     }
 }
